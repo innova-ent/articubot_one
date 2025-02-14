b55c14302f73aa9056501b287c01be084ddd1e9a
innova-ent <innova@iewireless.ca>
2025-02-14 13:49:43 -0600

    1st push

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -1,28 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+#from launch.substitutions import LaunchConfiguration
+#from launch.actions import DeclareLaunchArgument
 
 import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time')
+    #use_sim_time = LaunchConfiguration('use_sim_time')
 
     joy_params = os.path.join(get_package_share_directory('articubot_one'),'config','joystick.yaml')
 
     joy_node = Node(
             package='joy',
             executable='joy_node',
-            parameters=[joy_params, {'use_sim_time': use_sim_time}],
+            parameters=[joy_params], #{'use_sim_time': use_sim_time}],
          )
 
     teleop_node = Node(
             package='teleop_twist_joy',
             executable='teleop_node',
             name='teleop_node',
-            parameters=[joy_params, {'use_sim_time': use_sim_time}],
-            remappings=[('/cmd_vel','/cmd_vel_joy')]
+            parameters=[joy_params],# {'use_sim_time': use_sim_time}],
+    #        remappings=[('/cmd_vel','/cmd_vel_joy')]
          )
 
     # twist_stamper = Node(
@@ -35,10 +35,7 @@ def generate_launch_description():
 
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
+       ####     description='Use sim time if true'),
         joy_node,
         teleop_node,
         # twist_stamper       
